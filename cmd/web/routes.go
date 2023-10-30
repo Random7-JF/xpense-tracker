ab15f2ba6782855dfa31af2378aa4d421b488301
Jon Fingas <jon.fingas@gmail.com>
2023-10-29 20:06:35 -0500

    removed .html from air and added logging middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,11 +5,12 @@ import (
 	"github.com/Random7-JF/xpense-tracker/cmd/web/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func addRoutes() {
 	mw := middleware.NewMiddleware(&App)
-	App.Web.Use(mw.SetupSession())
+	App.Web.Use(mw.SetupSession(), logger.New())
 	App.Web.Get("/", handlers.Index)
 	App.Web.Get("/login", handlers.Login)
 	App.Web.Post("/login", handlers.PostLogin)
