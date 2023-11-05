e79f3458a1fc5e73577e7276ab07e530bee7c773
Jon Fingas <jon.fingas@gmail.com>
2023-11-05 01:00:41 -0500

    added test sql, cleanup html and css

---
+++ sql/test/dummyData.sql
@@ -0,0 +1,25 @@
+INSERT INTO expenses (label, amount, frequency, tag, expensedate, submissiondate, userid)
+VALUES 
+  ('Groceries', 50.0, 'Weekly', 'Food', '2023-11-01', '2023-11-01', '1'),
+  ('Rent', 1000.0, 'Monthly', 'Housing', '2023-11-01', '2023-11-01', '1'),
+  ('Internet Bill', 60.0, 'Monthly', 'Utilities', '2023-11-05', '2023-11-05', '1'),
+  ('Gasoline', 40.0, 'Bi-weekly', 'Transportation', '2023-11-03', '2023-11-03', '1'),
+  ('Eating Out', 30.0, 'Monthly', 'Food', '2023-11-07', '2023-11-07', '1'),
+  ('Gym Membership', 50.0, 'Monthly', 'Health', '2023-11-05', '2023-11-05', '1'),
+  ('Movies', 20.0, 'Once', 'Entertainment', '2023-11-08', '2023-11-08', '1'),
+  ('Books', 15.0, 'Once', 'Entertainment', '2023-11-09', '2023-11-09', '1'),
+  ('Clothing', 70.0, 'Once', 'Shopping', '2023-11-10', '2023-11-10', '1'),
+  ('Phone Bill', 45.0, 'Monthly', 'Utilities', '2023-11-02', '2023-11-02', '1');
+
+INSERT INTO expenses (label, amount, frequency, tag, expensedate, submissiondate, userid)
+VALUES 
+  ('Coffee', 5.0, 'Daily', 'Food', '2023-11-01', '2023-11-01', '2'),
+  ('Electricity Bill', 70.0, 'Monthly', 'Utilities', '2023-11-01', '2023-11-01', '2'),
+  ('Public Transportation', 30.0, 'Weekly', 'Transportation', '2023-11-05', '2023-11-05', '2'),
+  ('Books', 25.0, 'Once', 'Entertainment', '2023-11-03', '2023-11-03', '2'),
+  ('Fitness Class', 40.0, 'Weekly', 'Health', '2023-11-07', '2023-11-07', '2'),
+  ('Movie Night', 20.0, 'Monthly', 'Entertainment', '2023-11-05', '2023-11-05', '2'),
+  ('Home Decor', 60.0, 'Once', 'Shopping', '2023-11-08', '2023-11-08', '2'),
+  ('Internet Subscription', 50.0, 'Monthly', 'Utilities', '2023-11-09', '2023-11-09', '2'),
+  ('Eating Out', 35.0, 'Weekly', 'Food', '2023-11-10', '2023-11-10', '2'),
+  ('New Shoes', 65.0, 'Once', 'Shopping', '2023-11-02', '2023-11-02', '2');
